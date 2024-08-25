edb692a78b947c7a31ecaaee0b82ef3942dd5876
Kink3d <matthewdean3d@gmail.com>
2024-08-25 15:42:27 +0200

    First version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,12 +4,14 @@ All notable changes to this package are documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
-## [major.minor.patch] - yyyy-mm-dd
-### Added
-- Lorel Ipsum
-
+## [2.0.0] - 2020-01-27
 ### Changed
-- Lorel Ipsum
+- Moved to package
 
-### Fixed
-- Lorel Ipsum
+## [1.0.0] - 2017-12-01
+### Added
+- Toon Standard shader
+- Toon Water shader
+- Buoyancy controller
+- Demo scene
+- Source assets
---
+++ ShaderLibrary/LitInput.hlsl
@@ -0,0 +1,43 @@
+#ifndef ADVANCED_SHADING_LITINPUT_INCLUDED
+#define ADVANCED_SHADING_LITINPUT_INCLUDED
+
+// -------------------------------------
+// Includes
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+// -------------------------------------
+// Uniforms
+CBUFFER_START(UnityPerMaterial)
+float4 _BaseMap_ST;
+half4 _BaseColor;
+half4 _SpecColor;
+half4 _EmissionColor;
+half _Cutoff;
+half _Smoothness;
+half _Metallic;
+half _BumpScale;
+half _OcclusionStrength;
+half _Anisotropy;
+half _ClearCoat;
+half _ClearCoatSmoothness;
+half3 _SubsurfaceColor;
+half _Thickness;
+half _LocalMirror;
+CBUFFER_END
+
+TEXTURE2D(_OcclusionMap);       SAMPLER(sampler_OcclusionMap);
+TEXTURE2D(_MetallicGlossMap);   SAMPLER(sampler_MetallicGlossMap);
+TEXTURE2D(_SpecGlossMap);       SAMPLER(sampler_SpecGlossMap);
+TEXTURE2D(_AnisotropyMap);      SAMPLER(sampler_AnisotropyMap);
+TEXTURE2D(_DirectionMap);       SAMPLER(sampler_DirectionMap);
+TEXTURE2D(_ClearCoatMap);       SAMPLER(sampler_ClearCoatMap);
+TEXTURE2D(_SubsurfaceMap);      SAMPLER(sampler_SubsurfaceMap);
+TEXTURE2D(_ThicknessMap);       SAMPLER(sampler_ThicknessMap);
+
+TEXTURE2D(_ReflectionMap);      SAMPLER(sampler_ReflectionMap);
+TEXTURE2D(_LocalReflectionMap); SAMPLER(sampler_LocalReflectionMap);
+
+// -------------------------------------
+// Includes
+#include "Packages/com.deadfolk.advancedshading/ShaderLibrary/Input.hlsl"
+#endif
